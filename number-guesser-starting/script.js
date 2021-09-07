038f64e157db332565f519d01886984f4f51f242
JagritGill <72464099+JagritGill@users.noreply.github.com>
2021-09-07 12:40:15 -0700

    Add files via upload

---
+++ number-guesser-starting/script.js
@@ -0,0 +1,35 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+function generateTarget() {
+    // this will gengerate a number between 0 and 9 
+    return Math.floor(Math.random() * 10);
+}
+
+function compareGuesses(humanNum, computerNum, target) {
+    let compGuess = Math.abs(target - computerNum);
+    let humanGuess = Math.abs(target - humanNum);
+
+    if (humanNum > 9 || humanNum < 0)
+        alert('Please enter a number between 0 and 9');
+
+    if (humanGuess <= compGuess) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function updateScore(winner) {
+    if (winner === 'human') {
+        humanScore++;
+    } else if (winner === 'computer') {
+        computerScore++;
+    }
+}
+
+function advanceRound() {
+    currentRoundNumber++;
+}
+
