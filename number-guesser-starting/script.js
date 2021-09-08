c0afe995d6717075dadd1a8fbd5593d3fb7d30cc
JagritGill <72464099+JagritGill@users.noreply.github.com>
2021-09-08 10:15:08 -0700

    Update script.js
    just a update

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -8,19 +8,19 @@ function generateTarget() {
 }
 
 const getAbsoluteDistance = (num1, num2) => {
-    return Math.abs(num1 - num2)
+    return Math.abs(num1 - num2);
 }
 
 function compareGuesses(humanNum, computerNum, target) {
-    let compGuess = Math.abs(target - computerNum);
-    let humanGuess = Math.abs(target - humanNum);
+    
+    humanGuess = getAbsoluteDistance(humanNum, target);
+    compGuess = getAbsoluteDistance(computerNum, target);
 
     if (humanNum > 9 || humanNum < 0) {
         alert('Please enter a number between 0 and 9');
     }
 
-    human = getAbsoluteDistance(humanNum, target)
-    computer = getAbsoluteDistance(computerNum, target)
+
 
     if (humanGuess <= compGuess) {
         return true;
